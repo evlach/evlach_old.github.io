24b13c38849d57f1c4184539cc316f6214a5f31e
evlach <evangelos.vlachogiannis@gmail.com>
2014-10-22 11:47:38 +0200

    cr 2 - Keep the head clean + folder structure

--- courses/RWD_Oct14/assign03/js/script.js
+++ courses/RWD_Oct14/assign03/js/script.js
@@ -1,4 +1,14 @@
-Modernizr.load({
-    test: Modernizr.inputtypes.date && Modernizr.inputtypes.email,
-    nope: 'css/nohtml5.css'
-});
+(function () {
+    "use strict";
+    if (!Modernizr.inputtypes.date) {
+        var i = 0;
+        var html5onlyElements = document.getElementsByClassName("html5only");
+        for (i = 0; i < html5onlyElements.length; i++) {
+            html5onlyElements.item(i).style.display = "none";
+        }
+        var nohtml5Elements = document.getElementsByClassName("nohtml5");
+        for (i = 0; i < nohtml5Elements.length; i++) {
+            nohtml5Elements.item(i).style.display = "block";
+        }
+    }
+})();
